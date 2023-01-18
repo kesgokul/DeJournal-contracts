bb110e56cba012fdac8f91e9f1dce807f2926fcc
kesgokul <gokulkesavan5005@gmail.com>
2023-01-18 13:14:12 +0530

    introduce prospect function

--- README.md
+++ README.md
@@ -5,3 +5,24 @@
 To crerate some sort of a DAO that will function as a decetralized Journal where Academis, Scholars and Scientists can publich their research papers.
 It is to run by Academics for Academics in a transparent way.
 It aims to make the peer-review process transparent and directly incentiviced for the reviewers.
+
+## Workings
+
+1. Members recieve an ERC-721/soulbound token as a proof of membership which also gives them voting power.
+2. The token URI may contain details of their work (accademic achievements, impact, etc ).
+
+## Yet to figure out
+
+### Membership
+
+1. Who qualifies to become a member?
+2. How does one become a member?
+   - Do existing members need to vote on adding new members?
+   - Is it enough for any one existing member to refer and induct another one?
+   - Or a certain quorum needed to induct the referred?
+   - A one-time membership fee?
+
+### Proposing papers
+
+2. Does one need to be a member to submit (Propose) a Paper for review?
+3. What qualifies one to be able to perform reviews (only members)?
--- contracts/DeJournalGovernor.sol
+++ contracts/DeJournalGovernor.sol
@@ -3,42 +3,82 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./token/DeJournalToken.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+error DeJournalGovernor__alreadyInit();
+error DeJournalGovernor__referrerNotMember();
 
 contract DeJournalGovernor {
-    error DeJournalGovernor__AlreadyInit();
+    using SafeCast for uint256;
+    uint256 private constant PROSPECT_VOTING_DELAY = 1;
+    uint private constant PROSPECT_VOTING_PERIOD = 72000;
 
-    address[3] private s_initOwners;
+    address[3] private s_initMembers;
     bool private s_init = false;
-    mapping(address => bool) private s_isOwner;
+    mapping(address => bool) private s_isMember;
     DeJournalToken governanceTokenContract;
 
-    constructor(address[3] memory _owners, address _governanceTokenAddress) {
+    struct Prospect {
+        address prospectAddress;
+        address referrer;
+        bytes32 metadataHash;
+        uint256 voteStart;
+        uint256 deadline;
+        bool inducted;
+        bool denied;
+    }
+
+    mapping(uint256 => Prospect) private _prospects;
+
+    constructor(address[3] memory _members, address _governanceTokenAddress) {
         governanceTokenContract = DeJournalToken(_governanceTokenAddress);
-        s_initOwners = _owners;
+        s_initMembers = _members;
     }
 
     /*
     @notice the init function to initialize the founding members of the Journal DAO.
-            It can be called only ones. It is called by the deployer, post which the ownership
-            of the governance token contract will be transfere to this contract (governor).
+            It can be called only once. It is called by the deployer, post which the ownership
+            of the governance token contract will be transfered to this contract (governor).
     */
-    function initializeOwners() public {
+    function initializeMembers() public {
         if (s_init) {
-            revert DeJournalGovernor__AlreadyInit();
+            revert DeJournalGovernor__alreadyInit();
         }
-        address[3] memory owners = s_initOwners;
+        address[3] memory owners = s_initMembers;
         for (uint256 i = 0; i < owners.length; i++) {
             if (owners[i] != address(0)) {
-                s_isOwner[owners[i]] = true;
+                s_isMember[owners[i]] = true;
                 governanceTokenContract.mint(owners[i]);
             }
         }
         s_init = true;
     }
 
+    function introduceProspect(
+        address _prospectAddress,
+        bytes32 _metadataHash
+    ) public returns (uint256) {
+        // require(s_isMember[msg.msg.sender], "Referrer is not a Member");
+        if (!s_isMember[msg.sender]) {
+            revert DeJournalGovernor__referrerNotMember();
+        }
+
+        uint256 prospectId = uint256(
+            keccak256(abi.encode(_prospectAddress, _metadataHash))
+        );
+
+        Prospect storage prospect = _prospects[prospectId];
+        prospect.voteStart = block.number + PROSPECT_VOTING_DELAY;
+        prospect.deadline = prospect.voteStart + PROSPECT_VOTING_PERIOD;
+        prospect.prospectAddress = _prospectAddress;
+        prospect.referrer = msg.sender;
+
+        return prospectId;
+    }
+
     // view and pure getter functions
     function getInitOwners() public view returns (address[3] memory) {
-        return s_initOwners;
+        return s_initMembers;
     }
 
     function getGovernanceToken() public view returns (address) {
